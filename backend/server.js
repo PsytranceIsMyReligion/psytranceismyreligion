dd9f6c37dd466b2b8793684b19b5cf99f8ca69e9
Murray Furtado <murrayfurtado@gmail.com>
2019-08-29 10:05:01 +0100

    update mongo url

--- backend/server.js
+++ backend/server.js
@@ -33,9 +33,10 @@ app.use(
   })
 );
 
-mongoose.connect("mongodb://localhost:27017/test", {
+mongoose.connect("mongodb://psytrance:elena4me@cluster0-shard-00-00-0qnsy.mongodb.net:27017,cluster0-shard-00-01-0qnsy.mongodb.net:27017,cluster0-shard-00-02-0qnsy.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority", {
   useNewUrlParser: true
 });
+
 
 const connection = mongoose.connection;
 connection.once("open", () => {
